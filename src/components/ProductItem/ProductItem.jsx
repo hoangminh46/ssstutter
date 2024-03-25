9c10fbcec9906a674c9d5da00b4922b957db59b6
Hoang Cong Minh <minhhc@absoft.com.vn>
2024-03-25 11:01:38 +0700

    fix search input

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -2,12 +2,23 @@ import styles from './ProductItem.module.scss';
 import classNames from 'classnames/bind';
 import { Link } from 'react-router-dom';
 import { NumericFormat } from 'react-number-format';
+import { useDispatch } from 'react-redux';
+import { showSearchInput } from 'redux/configSlice';
 
 const cx = classNames.bind(styles);
 
 function ProductItems({ data, slideClass }) {
+  const dispatch = useDispatch();
+  function handleClick() {
+    dispatch(showSearchInput(false));
+  }
   return (
-    <Link to={`/product/${data.id}`} className={cx('product-item', slideClass && 'product-item__slider')} key={data.id}>
+    <Link
+      to={`/product/${data.id}`}
+      className={cx('product-item', slideClass && 'product-item__slider')}
+      key={data.id}
+      onClick={handleClick}
+    >
       <img src={data.image} alt="" className={cx('product-image')} />
       <p className={cx('product-name')}>{data.name}</p>
       <div className={cx('product-price')}>
